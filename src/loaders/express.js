53248f197e0d86a3af03ceb8f9efcf94fd465214
David Hdz <davidhernandeze@gmail.com>
2020-07-13 12:14:14 -0500

    add old index functionallity to the express loader

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,9 +1,32 @@
+import process from 'process'
+import stoppable from 'stoppable'
+import helmet from 'helmet'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import routes from '../api'
 import config from '../config'
+import logger from 'pino'
 
 export default ({ app }) => {
+  stoppable(app)
+  process.on('SIGINT', function onSigint () {
+    app.shutdown()
+  })
+  process.on('SIGTERM', function onSigterm () {
+    app.shutdown()
+  })
+  app.shutdown = function () {
+    app.stop(function onServerClosed (err) {
+      console.log(err)
+      console.log('asdd')
+      if (err) {
+        logger.error('An error occurred while closing the server: ' + err)
+        process.exitCode = 1
+      }
+    })
+    process.exit()
+  }
+
   /**
    * Health Check endpoints
    */
@@ -18,6 +41,7 @@ export default ({ app }) => {
   // It shows the real origin IP in the heroku or Cloudwatch logs
   app.enable('trust proxy')
   app.use(cors())
+  app.use(helmet())
 
   // "Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it."
   app.use(require('method-override')())
